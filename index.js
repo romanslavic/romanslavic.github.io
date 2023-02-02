f74e43bb12f48fb68a5b83350cfa30d92e8b4eb7
romanslavic <119129950+romanslavic@users.noreply.github.com>
2023-02-02 10:59:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ function playSound() {
 
 document.querySelector("button").addEventListener("click", function()  {
     playSound();
-    document.querySelector("img").classList.remove("image-hidden");
-    document.querySelector("button").classList.add("button-hidden");
+
+   document.querySelector("img").classList.remove("image-hidden");
+   document.querySelector("button").classList.add("button-hidden");
+    
 })
 
+setTimeout(function() {
+    document.querySelector("img").classList.add("image-hidden");
+document.querySelector("button").classList.remove("button-hidden");
+}, 10000);
